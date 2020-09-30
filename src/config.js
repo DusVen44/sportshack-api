e814339c6f8909421b12ec36fb621017f7d39e97
Dustin Venable <Venny44@yahoo.com>
2020-09-30 08:19:46 -0500

    added cors origin

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://venny@localhost/sportshack'
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://venny@localhost/sportshack',
+    CLIENT_ORIGIN: 'https://sportshack-client.vercel.app/'
   }
